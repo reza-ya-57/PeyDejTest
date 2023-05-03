ba898aa37f41c076c0ac97f5131dfed11b428575
reza <rezaayaari.57@gmail.com>
2023-05-03 12:55:57 +0330

    feat: change loading to the very simple version!

--- PeyDej/Models/Report/LoadingReportDetail.cs
+++ PeyDej/Models/Report/LoadingReportDetail.cs
@@ -1,17 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿//using System.ComponentModel.DataAnnotations;
+//using System.ComponentModel.DataAnnotations.Schema;
 
-namespace PeyDej.Models.Report;
+//namespace PeyDej.Models.Report;
 
-[Table(name: "LoadingReportDetail", Schema = "Report")]
-public class LoadingReportDetail
-{
-    [Key] public long Id { get; set; }
+//[Table(name: "LoadingReportDetail", Schema = "Report")]
+//public class LoadingReportDetail
+//{
+//    [Key] public long Id { get; set; }
 
-    public DateTime InsDate { get; set; } = DateTime.Now;
-    [Display(Name = "توع تیغه")]
-    public long BladKindId { get; set; }
-    public long? LoadingReportId { get; set; }
-    [Display(Name ="مقدار")]
-    public long? Value { get; set; }
-}
+//    public DateTime InsDate { get; set; } = DateTime.Now;
+//    [Display(Name = "توع تیغه")]
+//    public long BladKindId { get; set; }
+//    public long? LoadingReportId { get; set; }
+//    [Display(Name ="مقدار")]
+//    public long? Value { get; set; }
+//}
